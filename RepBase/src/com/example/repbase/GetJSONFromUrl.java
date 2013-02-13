c9c9f2d59e1306439e8dfb3afff7ed5ad948e3ed
chistyakov <al.ol.chistyakov@gmail.com>
2013-02-13 23:50:00 +0400

    The issue with parsing server's respond is solved
    Server returns big html code in case the error occurred. We have
    to read this data massive per line. BufferReader is used.
    Some additional information can be found in "workbook" of Klimenkov,
    Gavrilov and Copa on p. 71

--- RepBase/src/com/example/repbase/GetJSONFromUrl.java
+++ RepBase/src/com/example/repbase/GetJSONFromUrl.java
@@ -1,5 +1,6 @@
 package com.example.repbase;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -26,42 +27,32 @@ public class GetJSONFromUrl extends AsyncTask<String, Integer, JSONObject> {
 			DefaultHttpClient httpClient = new DefaultHttpClient();
 			HttpResponse response = httpClient.execute(request);
 			HttpEntity resp = response.getEntity();
-
-			char[] buffer = new char[(int) resp.getContentLength()];
+			
 			InputStream stream = resp.getContent();
-			InputStreamReader reader = new InputStreamReader(stream);
-			reader.read(buffer);
+			InputStreamReader reader = new InputStreamReader(stream, "utf-8");
+			
+			// BufferedReader is used for big blocks of data
+			BufferedReader br=new BufferedReader(reader);
+			
+			// we have to read respond per line
+			// because reader.read(buffer) sometimes places corrupted data into the buffer
+			// I didn't find the nature of such issue
+			// and used code from here: http://habrahabr.ru/qa/11449/
+			String strBuffer=null;
+			StringBuilder sb = new StringBuilder();
+			while ((strBuffer = br.readLine()) != null) {
+				sb.append(strBuffer);
+			}
+			strBuffer = sb.toString();
+			
 			stream.close();
+			reader.close();
+			br.close();
 
 			JSONObject result;
-			String strBuffer = String.valueOf(buffer);
 
-			Log.d("Auth", "server's respond: " + String.valueOf(buffer));
 			Log.d("Auth", "server's respond (the value of strBuffer): "
 					+ strBuffer);
-			// buffer sometimes is corrupted when error occurrences
-			// see log below:
-			// <style>BODY { color: #000000; background-color: white;
-			// font-family: Verdana; margin-left: 0px; margin-top: 0px; }
-			// #content { margin-left: 30px; font-size: .70em; padding-bottom:
-			// 2em; } A:link { color: #336699; font-weight: bold;
-			// text-decoration: underline; } A:visited { color: #6699cc;
-			// font-weight: bold; text-decoration: underline; } A:active {
-			// color: #336699; font-weight: bold; text-decoration: underline; }
-			// .heading1 { background-color: #003366; border-bottom: #336699 6px
-			// solid; color: #ffffff; font-family: Tahoma; font-size: 26px;
-			// font-weight: normal;margin: 0em 0em 10px -20px; padding-bottom:
-			// 8px; padding-left: 30px;padding-top: 16px;} pre {
-			// font-size:small; background-color: #e5e5cc; padding: 5px;
-			// font-family: Courier New; margin-top: 0px; border: 1px #f0f0e0
-			// solid; white-space: pre-wrap; white-space: -pre-wrap; word-wrap:
-			// break-word; } table { border-collapse: collapse; border-spacing:
-			// 0px; font-family: Verdana;} table th { border-right: 2px white
-			// solid; border-bottom: 2px white solid; font-weight: bold;
-			// backgr... THERE ARE BAD SIMBOLS FUTHER IN CP1251 ENCODING (just
-			// see log)
-			//
-			// so String.indexOf("The exception message is '") returns -1
 			try {
 				result = new JSONObject(strBuffer);
 			} catch (Exception ex) {
