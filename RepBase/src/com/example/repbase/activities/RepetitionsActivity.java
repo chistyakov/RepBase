a6bfc8676924a1c48e95df5b8b3664b8822920d0
chistyakov <al.ol.chistyakov@gmail.com>
2013-03-25 02:03:09 +0400

    one more attempt to clean out repository

--- RepBase/src/com/example/repbase/activities/RepetitionsActivity.java
+++ RepBase/src/com/example/repbase/activities/RepetitionsActivity.java
@@ -2,7 +2,6 @@ package com.example.repbase.activities;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
